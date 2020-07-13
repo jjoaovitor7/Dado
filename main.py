b15cdc10998308035fca1109f81a11a968a16aea
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2020-07-13 17:53:17 +0000

    lógica para rodar o dado novamente.

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ dado.setLado(qtde)
 
 print("Cor do Dado:", Dado.getCor(dado))
 print("Lado do Dado:", Dado.getLado(dado))
+
+while True:
+    rodar = input("Rodar o dado novamente?<S/n>\n:")
+    if rodar == "S" or cond == "s":
+        dado.setLado(qtde)
+        print("Lado do Dado:", Dado.getLado(dado))
+    else:
+        break
